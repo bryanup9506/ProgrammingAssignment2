ed17b21e501a9a0981e70dbabe033abaf4f0154e
bryanup9506 <bryan.up95@gmail.com>
2015-09-26 12:22:20 -0500

    Update cachematrix.R
    Corrections are welcome.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This two functions calculate the inverse matrix and avoid costly computations.
+## The first function stores a "special" matrix which will be stored. Later 
+## the second function will calculate the inverse matrix from the one stored.
 
-## Write a short comment describing this function
+## This function  helps to relationate a matrix given with a list of four 
+## functions, so we can disposse from them later.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<- NULL
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function()x
+  setInverse<- function(inversemat){
+    inv<<-inversemat
+  }
+  getInverse<- function()inv
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
+## This function will help to calculate the inverse matrix and to store it in 
+## the cache. The if loop checks if the inverse is already stored and if it is
+## true ends the function. If not, it calculates the inverse matrix. The for 
+## loop calculated the identity matrix so we can calculate the inverse matrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Assuming the given matrix is always invertible
+  inv<-x$getInverse()
+  if(!is.null(inv)){
+    return(inv)
+  }
+  data<-x$get()
+  a<-nrow(data)
+  b<-ncol(data)
+  z<-matrix(data=NA,a,b)
+  for(i in 1:a){
+    for(j in 1:b){
+      if(i==j){
+        z[i,j]=1
+      }
+      else{
+        z[i,j]=-0
+      }
+    }
+  }
+  inv<- solve(data,z)
+  x$setInverse(inv)
+  inv
 }
